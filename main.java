7599b23e55f59c2bf9e17f305cc544439ee8307f
CauanNewton <63312517+CauanNewton@users.noreply.github.com>
2020-04-07 16:19:25 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+package lab05;
+
+public class main {
+	public static void main(String[] args) {
+		CSVReader csv = new CSVReader();
+		csv.setDataSource("C:/Users/cauan/eclipse-workspace/lab05/src/lab05/arq001.csv");
+		String commands[] = csv.requestCommands();
+		System.out.println("Movimentos: " + commands[0] + " -> " + commands[1]);
+	}
+}
