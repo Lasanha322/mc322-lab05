cf9bcacb23e34d6f541e73efb88cac86aacb1958
CauanNewton <63312517+CauanNewton@users.noreply.github.com>
2020-04-07 20:02:20 -0300

    Update Campo.java

--- Campo.java
+++ Campo.java
@@ -1,13 +1,12 @@
 package lab05;
 
-
 public class Campo {
 	int x;
 	char y;
 	boolean peca; //false se for um espa�o em branco, true se for um espaco para pe�a
 	boolean comida; //false se a pe�a nesse espa�o n�o foi comida, true se foi comida
 	
-	Campo(int x, char y, boolean peca){
+	Campo (int x, char y, boolean peca) {
 		this.x = x;
 		this.y = y;
 		this.peca = peca;
@@ -16,15 +15,14 @@ public class Campo {
 		
 	void comerCampo() {
 		comida = true;
-	}	
+	}
+	
 	void mostrarCampo() {
-		if(peca==false) {
+		if (peca == false) {
 			System.out.println(" ");
-		}
-		if(peca==true && comida ==false) {
+		} else if (comida == false) {
 			System.out.println("P");
-		}
-		if(peca==true && comida ==true) {
+		} else {
 			System.out.println("-");
 		}
 	}
