a5c9453c8c3fd0060d6242058070cac1e36a80fb
b194966 <62315950+b194966@users.noreply.github.com>
2020-04-07 19:55:28 -0300

    Add files via upload

---
+++ Campo.java
@@ -0,0 +1,31 @@
+package lab05;
+
+
+public class Campo {
+	int x;
+	char y;
+	boolean peca; //false se for um espa�o em branco, true se tiver uma pe�a
+	boolean comida; //false se a pe�a nesse espa�o n�o foi comida, true se foi comida
+	
+	Campo(int x, char y, boolean peca){
+		this.x = x;
+		this.y = y;
+		this.peca = peca;
+		this.comida = false;
+	}
+		
+	void comerCampo() {
+		comida = true;
+	}	
+	void mostrarCampo() {
+		if(peca==false) {
+			System.out.println(" ");
+		}
+		if(peca==true && comida ==false) {
+			System.out.println("P");
+		}
+		if(peca==true && comida ==true) {
+			System.out.println("-");
+		}
+	}
+}
