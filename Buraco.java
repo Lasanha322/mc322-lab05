85a2c5372fe3db88c6ecbf0d7ee755648553c640
b194966 <62315950+b194966@users.noreply.github.com>
2020-04-07 17:12:04 -0300

    Add files via upload

---
+++ Buraco.java
@@ -0,0 +1,20 @@
+package lab05;
+
+public class Buraco {
+	int x;
+	String y;
+	int espaco; // se 0 espaço vago se 1 espaco com peca
+	
+	Buraco(int x, String y, int espaco){
+		this.x=x;
+		this.y=y;
+		this.espaco=espaco;
+	}
+	void mostrarBuraco() {
+		if(espaco == 1) {
+			System.out.println("P");
+		}
+		else
+			System.out.println("-");
+	}
+}
